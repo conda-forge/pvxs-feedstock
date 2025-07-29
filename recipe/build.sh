f5623ce2324b6099cff1abf516dc5cbcdfbded1c
Anders Lindh Olsson <anders.lindholsson@ess.eu>
2025-07-29 16:40:49 +0200

    Add macOS arm64 support
    Fix rpath for all binaries and libraries using install_name_tool.

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,11 @@ make install
 
 install -d ${PREFIX}/include/pvxs ${PREFIX}/bin/ ${PREFIX}/lib/ ${PREFIX}/pvxs/dbd ${PREFIX}/pvxs/db
 install bin/${EPICS_HOST_ARCH}/* ${PREFIX}/bin/
-install lib/${EPICS_HOST_ARCH}/*.so* ${PREFIX}/lib/
+if [[ "$target_platform" == osx* ]]; then
+  install lib/${EPICS_HOST_ARCH}/*.dylib* ${PREFIX}/lib/
+else
+  install lib/${EPICS_HOST_ARCH}/*.so* ${PREFIX}/lib/
+fi
 install -m 0664 include/pvxs/* ${PREFIX}/include/pvxs/
 install -m 0664 dbd/* ${PREFIX}/pvxs/dbd
 install -m 0664 db/* ${PREFIX}/pvxs/db
@@ -22,3 +26,21 @@ mkdir -p $PREFIX/etc/conda/deactivate.d
 cat <<EOF > $PREFIX/etc/conda/deactivate.d/pvxs_deactivate.sh
 unset PVXS
 EOF
+
+if [[ "$target_platform" == osx* ]]; then
+  # Fix install_name for all .dylib files in $PREFIX/lib
+  for lib in "${PREFIX}/lib/"*.dylib*; do
+    [ -e "$lib" ] || continue
+    install_name_tool -id "@rpath/$(basename "$lib")" "$lib"
+  done
+
+  # Patch all binaries and libraries in $PREFIX/bin and $PREFIX/lib to use @rpath
+  for bin in "${PREFIX}/bin/"* "${PREFIX}/lib/"*.dylib*; do
+    [ -e "$bin" ] || continue
+    otool -L "$bin" | awk '/libpvxs.*\.dylib/ {print $1}' | while read -r dep; do
+      [ -z "$dep" ] && continue
+      newname="@rpath/$(basename "$dep")"
+      install_name_tool -change "$dep" "$newname" "$bin" || true
+    done
+  done
+fi
