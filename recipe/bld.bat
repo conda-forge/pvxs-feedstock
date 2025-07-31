4678d76bc06cb739fa3da368917d17d2db5d7c5b
Anders Lindh Olsson <anders.lindholsson@ess.eu>
2025-07-31 10:56:47 +0200

    Fix missing DLL paths

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -21,12 +21,12 @@ if errorlevel 1 (
 mkdir "%PREFIX%\bin" "%PREFIX%\lib" ^
       "%PREFIX%\include\pvxs" "%PREFIX%\pvxs\dbd" "%PREFIX%\pvxs\db"
 
-copy "bin\%EPICS_HOST_ARCH%\*.exe" "%PREFIX%\bin\"   >nul
-copy "lib\%EPICS_HOST_ARCH%\*.dll" "%PREFIX%\bin\"   >nul
-copy "lib\%EPICS_HOST_ARCH%\*.lib" "%PREFIX%\lib\"   >nul
-copy "include\pvxs\*"              "%PREFIX%\include\pvxs\" >nul
-copy "dbd\*"                       "%PREFIX%\pvxs\dbd\"     >nul
-copy "db\*"                        "%PREFIX%\pvxs\db\"      >nul
+copy "bin\%EPICS_HOST_ARCH%\*.exe"              "%PREFIX%\bin\" >nul
+copy "bin\%EPICS_HOST_ARCH%\*.dll"              "%PREFIX%\bin\" >nul
+copy "lib\%EPICS_HOST_ARCH%\*.lib"              "%PREFIX%\lib\" >nul
+copy "include\pvxs\*"                           "%PREFIX%\include\pvxs\" >nul
+copy "dbd\*"                                    "%PREFIX%\pvxs\dbd\" >nul
+copy "db\*"                                     "%PREFIX%\pvxs\db\" >nul
 
 if not exist "%PREFIX%\etc\conda\activate.d" mkdir "%PREFIX%\etc\conda\activate.d"
 if not exist "%PREFIX%\etc\conda\deactivate.d" mkdir "%PREFIX%\etc\conda\deactivate.d"
